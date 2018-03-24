b1bbce58609ea5a9f645fa4f6abeeda72c588f89
Victor Hugo <vct.bta@gmail.com>
2018-03-24 11:46:52 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,44 @@
+# php-mysql-docker-generator
+Gera automaticamente a estrutura para a criação de ambientes web PHP, Apache e MySQL com docker-compose.
+
+## Requisitos
+ * Sistema que permita a execução de [shell scripts](https://pt.wikipedia.org/wiki/Shell_script)
+ * [Docker](https://www.docker.com/)
+ * [Docker Compose](https://docs.docker.com/compose/)
+
+## Instalação
+ * Dê permissão de execução ao arquivo installer.sh;
+ ```
+ chmod +x installer.sh
+ ```
+ * Execute o arquivo por meio do comando **./installer.sh**;
+ * O script irá instalar o comando gerador de ambientes após uma mensagem de confirmação;
+ * Após isso o comando **docker-gen-env** estará disponível. O comando pode ser executado sem parãmetros para a exibição da ajuda.
+ 
+## Atualização / desinstalação
+* Com o comando já disponível no sistema, execute o arquivo installer.sh por meio de **./installer.sh**;
+* Selecione a opção **1** para atualizar o script com a versão no diretório atual ou a opção **2** para remover o script do sistema.
+
+## Documentação
+ * Parâmetros obrigatórios:
+    * Título do projeto, define o título do diretório raíz e o nome dos containeres;
+    * Virtualhost apontando para o ambiente criado;
+    * Nome do banco MySQL gerado automaticamente;
+    * Senha do usuário root do MySQL.
+ * Parâmetros opcionais:
+    * Versão do PHP utilizada no ambiente. Padrão: **latest** (ver https://hub.docker.com/r/webgriffe/php-apache-base/tags/);
+    * Raíz do Apache: Diretório à partir de **/var/html/www** (diretório **public** na raíz da estrutura de diretórios criada).
+ * Exemplo:
+    * **docker-gen-env projeto-teste test.dev base-teste root 5.6 public**
+ * Ao executar o comando exbido acima, a seguinte estrutura será criada à partir do seu diretório atual (**você ainda pode prosseguir ou cancelar antes que a estrutura seja gerada**):
+  ```
+  - projeto-teste
+  |- public (raíz do apache do docker)
+  |- mysql (diretório de armazenamendo dos dados do mysql do docker)
+  |- docker-compose.yml (arquivo já com as configurações iniciais)
+  ```
+
+## Dicas importantes
+ * O MySQL deve ser referenciado pelo nome do container MySQL gerado automaticamente (definido pelo título do projeto seguido de **-db**) e não por localhost.
+ * Os containeres podem ser parados executando o comando **docker-compose stop**, reiniciados com o comando **docker-compose restart** e inicializados com o comando **docker-compose up -d** na raíz da estrutura de diretórios gerada.
+ * É possível verificar os containeres executando com o comando **docker ps**, verificar todos os containeres criados com **docker ps -a** e parar todos os containeres com **docker stop $(docker ps -q)**.
---
+++ wp-gen-env.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+if [ $# -lt 4 ]; then
+	echo '        __                                             __'
+	echo '.-----.|  |--.-----.______.--------.--.--.-----.-----.|  |'
+	echo '|  _  ||     |  _  |______|        |  |  |__ --|  _  ||  |'
+	echo '|   __||__|__|   __|      |__|__|__|___  |_____|__   ||__|'
+	echo '|__|         |__|                  |_____|        |__|'
+	echo '    __              __                                                          __'
+	echo '.--|  |.-----.----.|  |--.-----.----.______.-----.-----.-----.-----.----.---.-.|  |_.-----.----.'
+	echo '|  _  ||  _  |  __||    <|  -__|   _|______|  _  |  -__|     |  -__|   _|  _  ||   _|  _  |   _|'
+	echo '|_____||_____|____||__|__|_____|__|        |___  |_____|__|__|_____|__| |___._||____|_____|__|'
+	echo '                                           |_____|'
+	echo
+	echo 'Parâmetros esperados:'
+	echo ' - título do projeto'
+	echo ' - virtual host'
+	echo ' - nome do banco'
+	echo ' - senha do root'
+        echo ' - versão do php (opcional)'
+	echo ' - raíz do apache à partir de /var/html/www (opcional)'
+	echo
+	echo "Exemplo de comando completo: ${0##*/} projeto-teste test.dev base-teste root 5.6 public"
+	echo 'Repositório do projeto: https://github.com/VictorHugoBatista/wordpress-docker-generator'
+	exit 0
+fi
+
+project_title=$1
+virtual_host=$2
+database_name=$3
+root_password=$4
+php_version=`[ $5 ] && echo $5 || echo 'latest'`
+apache_root=`[ $6 ] && echo "$6" || echo ''`
+
+echo 'A seguinte estrutura será criada neste diretório:'
+echo " - $project_title"
+echo ' |- public (raíz do apache do docker)'
+echo ' |- mysql (dados armazenados pelo mysql do docker)'
+echo ' |- docker-compose.yml (gera os containeres do apache/php e do mysql e os relaciona)'
+echo ''
+echo 'Os seguintes containeres serão criados:'
+echo " - $project_title-web (php/apache)"
+echo "   - PHP $php_version"
+echo "   - Diretório raíz do apache: /var/www/html/$apache_root"
+echo "   - Virtual host: $virtual_host"
+echo " - $project_title-db (mysql)"
+echo "   - Nome do banco: $database_name"
+echo "   - Senha do root: $root_password"
+echo ''
+
+# Pede confirmação sobre a estrutura à ser criada
+continue_process=''
+while [ "$continue_process" = '' ] || [ "$continue_process" != 's' ] && [ "$continue_process" != 'n' ]; do
+	echo 'Pressione "s" ou "n" para prosseguir:'
+	read continue_process
+done
+
+# Para a execução caso a opção selecionada foi 'n'
+if [ "$continue_process" = 'n' ]; then
+	echo 'A operação foi cancelada!'
+	exit 0
+fi
+
+# Verifica a existência de um arquivo com o nome
+# sugerido para ser o diretório raíz do site. Se
+# sim, para o programa com status 1.
+if [ -e $project_title ]; then
+	echo "Um arquivo/diretório de nome $project_title já existe!"
+	exit 1
+fi
+
+# Gera a estrutura de arquivos e concede permissão
+mkdir $project_title
+cd $project_title
+mkdir public
+mkdir mysql
+touch docker-compose.yml
+
+# Popula docker-compose e adiciona os dados fornecidos no programa
+cat /usr/bin/docker-compose-sample.yml > docker-compose.yml
+sed -i "s/NOME-DO-PROJETO/$project_title/g" docker-compose.yml
+sed -i "s/VIRTUAL-HOST/$virtual_host/g" docker-compose.yml
+sed -i "s/BANCO/$database_name/g" docker-compose.yml
+sed -i "s/SENHA-ROOT/$root_password/g" docker-compose.yml
+sed -i "s/PHP-VERSION/$php_version/g" docker-compose.yml
+sed -i "s/APACHE-ROOT/$apache_root/g" docker-compose.yml
+
+# Exibe estrutura de arquivos
+echo 'Estrutura criada com sucesso:'
+ls -la
+echo ''
+
+if [ $(docker ps -a -q -f name="$project_title-web") ] || [ $(docker ps -a -q -f name="$project_title-db") ]; then
+	echo 'Já existem containeres com o mesmo nome da estrutura criada!'
+	exit 1
+fi
+
+chmod -R 777 .
+docker-compose up -d
+
+exit 0
+
